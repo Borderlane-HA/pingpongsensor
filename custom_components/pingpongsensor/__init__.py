280878e36ff3800fd236edfc1fdb8afbe05072a3
Borderlane-HA <Spam-Schutz@gmx.de>
2025-06-13 09:23:56 +0200

    Update __init__.py

--- custom_components/pingpongsensor/__init__.py
+++ custom_components/pingpongsensor/__init__.py
@@ -2,9 +2,9 @@ from .const import DOMAIN
 
 async def async_setup_entry(hass, entry):
     hass.async_create_task(
-       hass.config_entries.async_forward_entry_setups(entry, "sensor")
+        hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     )
     return True
 
 async def async_unload_entry(hass, entry):
-    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    return await hass.config_entries.async_unload_platforms(entry, ["sensor"])
